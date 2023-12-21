17900df76f766848476bef6db031c7f5d4c6f6c5
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-12-20 23:27:52 -0800

    started over

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import ReactDOM from "react-dom"; // Correct the import statement
+import StarryNight from "./components/Stars";
 import "./styles.css";
 
 // serves as the root component of the application
 const App = () => {
-  return <h1 className="text-red-500" >Hello World</h1>;
+  return (
+    <div>
+      <StarryNight></StarryNight>
+    </div>
+  ) 
 };
 
 // Render the app component using ReactDOM.render
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,70 +1,23 @@
-import React, { useEffect, useRef } from "react";
-import * as THREE from "three";
+import React from "react";
 
-const Stars = () => {
-  const containerRef = useRef();
-  const windowHalf = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-
-  useEffect(() => {
-    // Create a scene, camera, and renderer
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-
-    containerRef.current.appendChild(renderer.domElement);
-
-    // Load the image (space1.jpg)
-    const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load("/components/nebula1.jpg");
-
-    // Create a sphere with the image as a material
-    const geometry = new THREE.SphereGeometry(5, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-    const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
-
-    // Set the camera position
-    camera.position.set(0, 0, 20);
-
-    // Render loop with automatic rotation
-    const animate = () => {
-      // Rotate the sphere on its own
-      sphere.rotation.x += 0.0035; // Adjust the rotation speed as needed
-      sphere.rotation.y += 0.0035;
-
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    };
-
-    animate();
-
-    // Handle window resize
-    const handleResize = () => {
-      const { innerWidth, innerHeight } = window;
-      camera.aspect = innerWidth / innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(innerWidth, innerHeight);
-      windowHalf.x = innerWidth / 2;
-      windowHalf.y = innerHeight / 2;
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
+const StarryNight = ({ children }) => {
   return (
-      <div ref={containerRef} />
+    <div className="relative w-full h-screen">
+      <div
+        className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-black bg-repeat bg-center z-0"
+        style={{ backgroundImage: 'url("stars.png")' }}
+      ></div>
+      <div
+        className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-transparent bg-repeat bg-center z-1 animate-move-twinkle-back"
+        style={{ backgroundImage: 'url("twinkling.png")' }}
+      ></div>
+      <div
+        className="absolute top-0 left-0 right-0 bottom-0 w-full h-full clouds bg-clouds opacity-40 animate-move-clouds"
+        style={{ backgroundImage: 'url("clouds.png")' }}
+      ></div>
+      {children}
+    </div>
   );
 };
 
-export default Stars;
+export default StarryNight;
